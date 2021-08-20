80331a9a02bdd263aeb63343238193b30c005e0d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-08-20 13:36:49 +0000

    Agrega los métodos size y union a la clase DinoSet

--- 01. Introduction/dinoset/main.cpp
+++ 01. Introduction/dinoset/main.cpp
@@ -4,10 +4,18 @@
 int main()
 {
     DinoSet a;
+    DinoSet b;
+    DinoSet c;
+
     a.add(DinoId::velocirraptor);
     a.add(DinoId::triceratops);
-    std::cout << a.to_string() << std::endl;
-    std::cout << a.contains(DinoId::velocirraptor) << std::endl;
-    std::cout << a.contains(DinoId::tiranosaurio) << std::endl;
+    b.add(DinoId::triceratops);
+    b.add(DinoId::pteranodon);
+
+    c = a + b;
+
+    std::cout << c.to_string() << std::endl;
+    std::cout << c.size() << std::endl;
+
     return 0;
 }
