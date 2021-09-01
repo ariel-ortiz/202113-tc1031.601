4065a1ef52d3e46c5ac710f892d9d67431b4c85e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-09-01 16:24:33 +0000

    Agrega código para demostrar el uso de apuntadores

--- 02. Referencias y apuntadores/ejem_apuntadores.cpp
+++ 02. Referencias y apuntadores/ejem_apuntadores.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+int global;
+
+void f(int *ptr) {
+    ++(*ptr);
+}
+
+void g(int x) {
+    ++x;
+}
+
 int main()
 {
     int i;  // i es un entero
@@ -8,9 +18,34 @@ int main()
     i = 5;
     p = &i; // p apunta a la dirección de i
 
-    std::cout << "i = " << i << std::endl;
-    std::cout << "p = " << p << std::endl;
+    std::cout << "i  = " << i << std::endl;
+    std::cout << "p  = " << p << std::endl;
     std::cout << "*p = " << *p << std::endl;
+    std::cout << "&p = " << &p << std::endl;
+    std::cout << "sizeof(i) = " <<  sizeof(i) << std::endl;
+    std::cout << "sizeof(p) = " <<  sizeof(p) << std::endl;
+
+    ++(*p);
+
+    ++p; // aritmética de apuntadores
+
+    std::cout << "i  = " << i << std::endl;
+    std::cout << "p  = " << p << std::endl;
+    std::cout << "*p = " << *p << std::endl;
+
+    std::cout << "global = " << global << std::endl;
+    std::cout << "&global = " << &global << std::endl;
+
+    p = nullptr;
+    std::cout << "p  = " << p << std::endl;
+    // std::cout << "*p = " << *p << std::endl; // Produce segmentation fault
+
+    int a = 10;
+    std::cout << "a = " << a << std::endl;
+    g(a);
+    std::cout << "a = " << a << std::endl;
+    f(&a);
+    std::cout << "a = " << a << std::endl;
 
     return 0;
 }
