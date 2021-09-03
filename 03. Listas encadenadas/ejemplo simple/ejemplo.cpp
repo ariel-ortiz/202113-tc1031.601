24ce8613b8673af290c59a3962d0e7d3773dd89b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-09-03 16:05:18 +0000

    Agrega funciones insert_first y print_list para listas encadenadas

--- 03. Listas encadenadas/ejemplo simple/ejemplo.cpp
+++ 03. Listas encadenadas/ejemplo simple/ejemplo.cpp
@@ -5,24 +5,32 @@ struct Node {
     Node *next;
 };
 
+Node* insert_first(Node *first, int value)
+{
+    Node *new_node = new Node;
+    new_node->value = value;
+    new_node->next = first;
+    return new_node;
+}
+
+void print_list(Node *first)
+{
+    while (first != nullptr) {
+        std::cout << first->value << " ";
+        first = first->next;
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
-    Node *lst;
-
-    lst = new Node;
-    lst->value = 5;
-    lst->next = nullptr;
-
-    Node *other;
-
-    other = new Node;
-    other->value = 10;
-    other->next = nullptr;
-
-    lst->next = other;
-
-    std::cout << lst->value << " " << lst->next->value <<std::endl;
-
-    delete lst;
-    delete other;
+    Node *lst = nullptr;
+    lst = insert_first(lst, 5);
+    lst = insert_first(lst, 10);
+    lst = insert_first(lst, 15);
+    print_list(lst);
+    lst = insert_first(lst, 20);
+    lst = insert_first(lst, 30);
+    print_list(lst);
+    return 0;
 }
