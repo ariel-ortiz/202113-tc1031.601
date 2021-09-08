a15a3d1ebc4fa673e2b30e1827a3602e036f1aee
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-09-08 15:29:06 +0000

    Agrega funciones free_list, size, sum y dup para listas encadenadas

--- 03. Listas encadenadas/ejemplo simple/ejemplo.cpp
+++ 03. Listas encadenadas/ejemplo simple/ejemplo.cpp
@@ -5,6 +5,7 @@ struct Node {
     Node *next;
 };
 
+// Complejidad: O(1)
 Node* insert_first(Node *first, int value)
 {
     Node *new_node = new Node;
@@ -13,6 +14,8 @@ Node* insert_first(Node *first, int value)
     return new_node;
 }
 
+
+// Complejidad: O(N)
 void print_list(Node *first)
 {
     while (first != nullptr) {
@@ -22,15 +25,70 @@ void print_list(Node *first)
     std::cout << std::endl;
 }
 
+// Complejidad: O(N)
+void free_list(Node *first)
+{
+    Node *current = first;
+    while (first != nullptr) {
+        first = first->next;
+        delete current;
+        current = first;
+    }
+}
+
+// Complejidad: O(N)
+int size(Node *first)
+{
+    int count = 0;
+    while (first != nullptr) {
+        ++count;
+        first = first->next;
+    }
+    return count;
+}
+
+// Comlejidad: O(N)
+int sum(Node *first)
+{
+    int total = 0;
+    while (first != nullptr) {
+        total += first->value;
+        first = first->next;
+    }
+    return total;
+}
+
+// Comlejidad: O(N)
+void dup(Node *first)
+{
+    while (first != nullptr) {
+        // Node *new_node = new Node;
+        // new_node->value = first->value;
+        // new_node->next = first->next;
+        // first->next = new_node;
+        first->next = insert_first(first->next, first->value);
+        first = first->next->next;
+    }
+}
+
 int main()
 {
     Node *lst = nullptr;
     lst = insert_first(lst, 5);
     lst = insert_first(lst, 10);
     lst = insert_first(lst, 15);
+    std::cout << "size = " << size(lst) << std::endl;
+    std::cout << "sum = " << sum(lst) << std::endl;
     print_list(lst);
     lst = insert_first(lst, 20);
     lst = insert_first(lst, 30);
+    std::cout << "size = " << size(lst) << std::endl;
+    std::cout << "sum = " << sum(lst) << std::endl;
     print_list(lst);
+    dup(lst);
+    std::cout << "size = " << size(lst) << std::endl;
+    std::cout << "sum = " << sum(lst) << std::endl;
+    print_list(lst);
+    free_list(lst);
     return 0;
 }
