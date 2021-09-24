84700dd05139433ed4e51414b9f4641704276df1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-09-24 15:59:21 +0000

    Agrega el código de postfix_evaluation

--- 04. Pilas y filas/main.cpp
+++ 04. Pilas y filas/main.cpp
@@ -3,5 +3,6 @@
 
 int main()
 {
+    std::cout << postfix_evaluation("2 2 +") << std::endl;
     return 0;
 }
--- 04. Pilas y filas/stacks_and_queues.cpp
+++ 04. Pilas y filas/stacks_and_queues.cpp
@@ -1,8 +1,76 @@
+#include <cctype>
+#include <queue>
+#include <regex>
+#include <stack>
+#include <stdexcept>
+#include <string>
 #include "stacks_and_queues.h"
+
+std::queue<std::string> split(const std::string& input)
+{
+    std::regex exp(R"((\d+)|(\s)|(.))");
+    std::smatch match;
+    std::string next_input = input;
+    std::queue<std::string> result;
+
+    while(regex_search(next_input, match, exp)) {
+        if (match[2] == "") {
+            result.push(match[0]);
+        }
+        next_input = match.suffix();
+    }
+
+    return result;
+}
 
 int postfix_evaluation(const std::string& expr)
 {
-    return 0;
+    std::queue<std::string> tokens = split(expr);
+    std::stack<int> eval_stack;
+
+    while (not tokens.empty()) {
+        std::string element = tokens.front();
+        tokens.pop();
+
+        if (isdigit(element[0])) {
+            eval_stack.push(stoi(element));
+
+        } else if (element == "+" or element == "-"
+            or element == "*" or element == "/") {
+            if (eval_stack.size() < 2) {
+                throw std::invalid_argument(expr
+                    + " too few operands for operator " + element);
+            }
+            int b = eval_stack.top();
+            eval_stack.pop();
+            int a = eval_stack.top();
+            eval_stack.pop();
+
+            int result;
+
+            if (element == "+") {
+                result = a + b;
+            } else if (element == "-") {
+                result = a - b;
+            } else if (element == "*") {
+                result = a * b;
+            } else {
+                result = a / b;
+            }
+            eval_stack.push(result);
+
+        } else {
+            throw std::invalid_argument(expr
+                + ": unknown symbol " + element);
+        }
+    }
+
+    if (eval_stack.size() == 1) {
+        return eval_stack.top();
+    } else {
+        throw std::invalid_argument(expr
+            + ": missing one or more operators");
+    }
 }
 
 bool balanced_brackets(const std::string& expr)
