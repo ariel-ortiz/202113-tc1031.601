ca266be6ecd910301badbba60733e78c63252247
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2021-11-19 16:33:06 +0000

    Agrega código que demuesta el uso de funciones anónimas en recorridos de árboles

--- 10. Árboles binarios/treeset/main.cpp
+++ 10. Árboles binarios/treeset/main.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include "treeset.h"
 
-void visita(int value)
-{
-    std::cout << value << ' ';
-}
-
 int main()
 {
     TreeSet<int> t {10, 5, 15, 13, 20, 14};
 
-    t.levelorder(visita);
+    t.levelorder([&] (int value) { std::cout << value << ' '; });
 
     std::cout << '\n';
+
+    int suma = 0;
+
+    t.levelorder([&] (int value) { suma += value; });
+
+    std::cout << suma << '\n';
 
     return 0;
 }
